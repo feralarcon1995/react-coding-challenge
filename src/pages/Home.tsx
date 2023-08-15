214cc5e5f0288ac788f50a8220d7702748deccb9
Fernando Alarcon <feralarcon1995@gmail.com>
2023-08-15 18:45:35 -0300

    change title hover

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,10 +11,10 @@ const Home = () => {
             <section className="fade grid justify-items-center grid-flow-row-dense xl:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1  grid-rows-3 p-10 gap-10" >
                 {loading ? <><Spinner /></> :
                     <>
-                        <Link to="/series" className=" flex items-end max-w-sm rounded overflow-hidden bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg h-80 font-bold text-xl mb-2 p-4 duration-150 hover:bg-gray-100" >
+                        <Link to="/series" className=" flex items-end max-w-sm rounded overflow-hidden bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg h-80 font-bold text-xl mb-2 p-4 duration-150 hover:bg-gray-100 hover:scale-125" >
                             Popular Seriess
                         </Link>
-                        <Link to="/movies" className=" flex items-end max-w-sm rounded overflow-hidden bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg h-80 font-bold text-xl mb-2 p-4 duration-150 hover:bg-gray-100" >
+                        <Link to="/movies" className=" flex items-end max-w-sm rounded overflow-hidden bg-gradient-to-r from-sky-500 to-indigo-500 shadow-lg h-80 font-bold text-xl mb-2 p-4 duration-150 hover:bg-gray-100 hover:scale-125" >
                             Popular Movies
                         </Link>
 
